aa096c94bcd1954c37f8658c319a92b638791416
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2021-12-10 22:36:34 +0000

    T1MF_interface

---
+++ src/type1/sets/T1MF_Interface.py
@@ -0,0 +1,73 @@
+
+"""
+T1MF_Interface.py
+Created 10/12/2021
+"""
+
+from generic.MF_Interface import MF_Interface
+from generic.Tuple import Tuple
+
+class T1MF_Interface(MF_Interface):
+    """
+    Class T1MF_Interface
+    An interface for type 1 membership functions
+
+    Parameters: None
+
+    Functions:
+        getFS
+        getAlphaCut
+        getPeak
+        getName
+        setName
+        getSupport
+        setSupport
+        setLeftShoulder
+        setRightShoulder
+        isLeftShoulder
+        isRightShoulder
+        getDefuzzifiedCentroid
+        getDefuzzifiedCOS
+    """
+
+    def getFS(self,x) -> float:
+        pass
+    
+    def getAlphaCut(self,alpha) -> Tuple:
+        pass
+
+    def getPeak() -> float:
+        pass    
+
+    def getName(self) -> str:
+        pass
+
+    def setName(self, name) -> None:
+        pass
+
+    def getSupport(self) -> Tuple:
+        pass
+
+    def setSupport(self, support) -> None:
+        pass
+
+    def setLeftShoulder(self, value) -> None:
+        pass
+
+    def setRightShoulder(self, value) -> None:
+        pass    
+
+    def isLeftShoulder(self) -> bool:
+        pass
+
+    def isRightShoulder(self) -> bool:
+        pass
+
+    def getDefuzzifiedCentroid(self, numberOfDiscretizations) -> float:
+        pass
+
+    def getDefuzzifiedCOS(self) -> float:
+        pass
+
+
+
