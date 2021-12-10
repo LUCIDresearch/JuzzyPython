22f65aab2831a4d66bd2b3f555a06984cde36a25
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2021-12-10 19:59:53 +0000

    Create output class

---
+++ src/generic/Output.py
@@ -0,0 +1,87 @@
+
+"""
+Output.py
+Created 10/12/2021
+"""
+import Tuple
+from typing import List
+
+class Output: 
+    """
+    Class Output: 
+    Enables the labeling of an output and captures the allowable domain for a given output.
+
+    Parameters:
+        name = name of output (String)
+        domain = (Tuple)
+        discretisationLevel = (Int) (Optional)
+
+    Functions: 
+        getName
+        setName
+        getDiscretisationLevel
+        setDiscretisationLevel
+        getDomain
+        setDomain
+        getDiscretisations
+    """
+
+    def __init__(self,name,domain,discretisationLevel = None) -> None:
+        self.discretisationLevel = 100
+        self.name = name
+        self.domain = domain
+        self.discretisedDomain = None
+        if discretisationLevel != None:
+            self.discretisationLevel = discretisationLevel
+    
+    def getName(self) -> str:
+        """Return the name of the output"""
+        return self.name
+    
+    def setName(self,name) -> None:
+        """Set the name of the output"""
+        self.name = name
+    
+    def getDiscretisationLevel(self) -> int:
+        """Get the current discretisation level"""
+        return self.discretisationLevel
+    
+    def setDiscretisationLevel(self,level) -> None:
+        """Set a new discretisation level"""
+        self.discretisationLevel = level
+    
+    def getDomain(self) -> Tuple:
+        """Get the current domain tuple"""
+        return self.domain
+    
+    def setDomain(self,domain):
+        """Set a new domain"""
+        self.domain = domain
+    
+    def getDiscretisations(self) -> List[float]:
+        """Returns an array with discrete values over the domain of this output. This 
+        discrete array is buffered in the Output object, i.e. if the same discretisation 
+        is kept, it is efficient to use the array from the output object (e.g. in rule-based
+        inference)."""
+        if self.discretisedDomain == None or len(self.discretisedDomain) != self.discretisationLevel:
+            self.discretisationDomain = [0] * self.discretisationLevel
+            stepsize = self.domain.getSize()/(self.discretisationLevel-1.0)
+            self.discretisationDomain[0] = self.domain.getLeft()
+            self.discretisationDomain[self.discretisationLevel-1] = self.domain.getRight()
+            for i in range(1,self.discretisationLevel-1):
+                self.discretisationDomain[i] = self.domain.getLeft()+i*stepsize
+        else:
+            return self.discretisationDomain
+    
+    def compareTo(self,o) -> int:
+        """Enables simple name-based ordering of outputs.
+        This method is solely used to maintain an ordering of outputs.
+        Compare the value of the output names and return an int from -1 to 1"""
+        if self.getName() < o.getName():
+            return -1
+        elif  self.getName() < o.getName():
+            return 1
+        else:
+            return 0
+
+
--- src/generic/Tuple.py
+++ src/generic/Tuple.py
@@ -1,30 +1,33 @@
+
+"""
+Tuple.py
+Created 10/12/2021
+"""
 from __future__ import annotations
 
 class Tuple:
     """
-    Created 10/12/2021
-
     Class Tuple: 
     Creates an object of two values related in some way, such as range.
     The left and right values are placed into a list
 
     Parameters:
-        l = The left value in float
-        r = The right value in float
+        l = The left value in (float) (Optional)
+        r = The right value in (float) (Optional)
     
     Functions: 
-        clone = Creates a duplicate copy of the tuple object
-        setLeft = Set the left value
-        setRight = Set the right value 
-        setEqual = Copy a tuple into the current object
-        getLeft = Return the left value
-        getRight = Return the right value
-        getAverage = Return the average of left and right
-        contains = Returns true if the parameter falls within the interval defined by the Tuple
-        getSize = Returns the difference of the right vs left values
-        toString = Returns a string of the current tuple
-        add = Add the values of another tuple onto the current one
-        compareTo = Compare tuple values of another object and return an int
+        clone
+        setLeft 
+        setRight 
+        setEqual 
+        getLeft 
+        getRight 
+        getAverage 
+        contains 
+        getSize 
+        toString 
+        add 
+        compareTo 
     """
 
     def __init__(self,l = None,r = None) -> None:
@@ -34,40 +37,52 @@ class Tuple:
             self.tuple[1] = r
 
     def clone(self) -> Tuple:
+        """Creates a duplicate copy of the tuple object"""
         return Tuple(self.getLeft(),self.getRight())
     
     def setLeft(self,l) -> None:
+        """Set the left value"""
         self.tuple[0] = l
     
     def setRight(self,r) -> None:
+        """Set the right value"""
         self.tuple[1] = r
     
     def setEqual(self,t) -> None:
+        """Copy a tuple into the current object"""
         self.tuple[0] = t.getLeft()
         self.tuple[1] = t.getRight()
     
     def getLeft(self) -> float:
+        """Return the left value"""
         return self.tuple[0]
 
     def getRight(self) -> float:
+        """Return the right value"""
         return self.tuple[1]
     
     def getAverage(self) -> float:
+        """Return the average of left and right"""
         return (sum(self.tuple))/2.0
     
     def contains(self,x) -> bool:
+        """Returns true if the parameter falls within the interval defined by the Tuple"""
         return (x >= self.getLeft() and x <= self.getRight())
 
     def getSize(self) -> float:
+        """Returns the difference of the right vs left values"""
         return self.getRight() - self.getLeft()
     
     def toString(self) -> str:
+        """Returns a string of the current tuple"""
         return "left = "+str(self.getLeft())+" and right = "+str(self.getRight())
     
     def add(self,x) -> Tuple:
+        """Add the values of another tuple onto the current one"""
         return Tuple(self.getLeft()+x.getLeft(),self.getRight()+x.getRight())
     
     def compareTo(self,o) -> int:
+        """Compare tuple values of another object and return an int"""
         if self.getRight() < o.getRight():
             return -1
         elif self.getRight() > o.getRight():
