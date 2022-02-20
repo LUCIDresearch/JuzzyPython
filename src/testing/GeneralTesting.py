e9fe3081e7d84d3a4d70dbd1b73b3a2b2a146693
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2022-02-20 14:21:50 +0000

    General testing

--- src/testing/GeneralTesting.py
+++ src/testing/GeneralTesting.py
@@ -9,10 +9,10 @@ from type1.sets.T1MF_Trapezoidal import T1MF_Trapezoidal
 from type1.sets.T1MF_Meet import T1MF_Meet
 from generic.Tuple import Tuple
 
-test = T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,16),Tuple(9,50)])
-print(test.writeToFileHighRes("test.txt",10))
+#test = T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,16),Tuple(9,50)])
+#print(test.writeToFileHighRes("test.txt",10))
 #test = T1MF_Union(T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]),T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]))
 #print(test.getSupport().toString())
 #test = T1MF_Trapezoidal("test",[1.0,2.0,3.0,4.0])
-#test = T1MF_Meet(T1MF_Discretized("Test",[Tuple(0,5),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]),T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,18),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]))
-#print(test.getAlphaCut(3).toString())
+test = T1MF_Meet(T1MF_Discretized("Test",[Tuple(0,5),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]),T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,18),Tuple(10,20),Tuple(2,30),Tuple(4,18),Tuple(9,50)]))
+print(test.findMax(T1MF_Trapezoidal("test",[1.0,2.0,3.0,4.0])))
