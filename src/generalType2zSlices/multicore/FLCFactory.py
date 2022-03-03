f6c188b3e740f0565f5845f755e229e5fb583e26
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2022-03-03 13:49:57 +0000

    More multiprocessing additions

--- src/generalType2zSlices/multicore/FLCFactory.py
+++ src/generalType2zSlices/multicore/FLCFactory.py
@@ -4,12 +4,13 @@ Created 19/2/2022
 """
 import sys
 sys.path.append("..")
-
+from generalType2zSlices.multicore.FLCPlant import FLCPlant
 from generic.Input import Input
 from generic.Output import Output
 from generic.Tuple import Tuple
 from intervalType2.system.IT2_Rulebase import IT2_Rulebase
 from typing import List, OrderedDict
+import multiprocessing
 
 class FLCFactory():
     """
@@ -21,11 +22,13 @@ class FLCFactory():
 
     """ 
 
-    def __init__(self,rulebases) -> None:
+    def __init__(self,rulebases: List[IT2_Rulebase]) -> None:
+        self.manager - multiprocessing.Manager()
         self.rulebases = rulebases
         self.weight = 0.0
         self.defaultTypeReduction = 1
         self.numberOfThreads = len(rulebases)
+        self.pool = multiprocessing.Pool(self.numberOfThreads)
         self.plants = [None] * self.numberOfThreads
         self.zLevels = [0.0] * self.numberOfThreads
 
@@ -33,7 +36,42 @@ class FLCFactory():
             self.zLevels[i] = (i+1.0) / self.numberOfThreads
             self.weight += self.zLevels[i]
         
-        self.rawReseults = OrderedDict()
+        self.rawResults = self.manager.dict()
         out = self.rulebases[0].getOutputs()
         for o in out:
+            self.rawResults[o] = [ [0]*self.numberOfThreads for i in range(2)]
+        for i in self.numberOfThreads:
+            self.plants[i] = FLCPlant(self.rulebases[i],self.rawResults,i,self.defaultTypeReduction)
+    
+    def runFactory(self, typeReductionType: int) -> dict:
+        returnValue = OrderedDict()
+        out = self.rulebases[0].getOutputs()
+        for o in out:
+            objs = self.rawResults[o]
+            objs[0] = [0] * self.numberOfThreads
+            objs[1] = [0] * self.numberOfThreads
+        
+        if not typeReductionType == self.defaultTypeReduction:
+            for p in self.plants:
+                p.setTypeReductionType(typeReductionType)
+            self.defaultTypeReduction = typeReductionType
+        
+        for i in range(self.numberOfThreads):
+            self.pool.apply(self.plants[i].run())
+
+        out = self.rulebases[0].getOutputs()
+        for o in out:
+            returnValue[o] = 0.0
+        
+        for i in range(self.numberOfThreads):
+            out = self.rulebases[0].getOutputs()
+            for o in out:
+                if self.rawResults[o][0][i] != None:
+                    returnValue[o] = returnValue.get(o) + self.rawResults[o][0][i].getAverage()*self.zLevels[i]
+        
+        out = self.rulebases[0].getOutputs()
+        for o in out:
+            returnValue[o] = returnValue[o] / self.weight
+
+        return returnValue
 
--- src/testing/GeneralTesting.py
+++ src/testing/GeneralTesting.py
@@ -25,6 +25,11 @@ from type1.sets.T1MF_Triangular import T1MF_Triangular
 from type1.sets.T1MF_Trapezoidal import T1MF_Trapezoidal
 from type1.sets.T1MF_Gaussian import T1MF_Gaussian
 from type1.sets.T1MF_Meet import T1MF_Meet
+from generalType2zSlices.system.GenT2_Antecedent import GenT2_Antecedent
+from generalType2zSlices.system.GenT2_Consequent import GenT2_Consequent
+from generic.Input import Input
+from generic.Output import Output
+from generalType2zSlices.system.GenT2_Rule import GenT2_Rule
 from generic.Tuple import Tuple
 
 test = T1MF_Discretized("Test",[Tuple(0,10),Tuple(5,16),Tuple(10,20),Tuple(2,30),Tuple(4,16),Tuple(9,50)])
@@ -48,8 +53,44 @@ disc = GenT2MF_Discretized(lowTipMF,4)
 #cyl = GenT2MF_CylExtension(test,4)
 defuzz = GenT2Engine_Defuzzification(4)
 inter = GenT2Engine_Intersection()
-inte =  inter.getIntersection(lowTipMF,lowTipMF)
-print(inte.toString())
+#inte =  inter.getIntersection(lowTipMF,lowTipMF)
+#print(inte.toString())
 #print(defuzz.typeReduce_standard(lowTipMF,4,4))
 #print(cyl.getFS(4).toString())
+numberOfzLevels = 4
+typeReduction = 0
+xDiscs = 50
+yDiscs = 10
 
+#Inputs to the FLS
+food = Input("Food Quality",Tuple(0,10)) #Rating from 0-10
+service = Input("Service Level",Tuple(0,10)) #Rating from 0-10
+#Output
+tip = Output(("Tip"),Tuple(0,30)) #Tip from 0-30%
+
+#Set up the membership functions (MFs) for each input and output
+badFoodUMF = T1MF_Triangular("Upper MF for bad food",0.0,0.0,10.0)
+badFoodLMF = T1MF_Triangular("Lower MF for bad food",0.0,0.0,8.0)
+badFoodIT2MF = IntervalT2MF_Triangular("IT2MF for bad food",badFoodUMF,badFoodLMF)
+badFoodMF = GenT2MF_Triangular("zGT2MF for bad food", primer = badFoodIT2MF, numberOfzLevels = numberOfzLevels)
+badFood = GenT2_Antecedent("BadFood",badFoodMF, food)
+lowTipUMF = T1MF_Gaussian("Upper MF Low tip", 0.0, 6.0)
+lowTipLMF = T1MF_Gaussian("Lower MF Low tip", 0.0, 4.0)
+lowTipIT2MF = IntervalT2MF_Gaussian("IT2MF for Low Tip",lowTipUMF,lowTipLMF)
+lowTipMF = GenT2MF_Gaussian("zGT2MF for Low tip", lowTipIT2MF, numberOfzLevels)
+lowTip =  GenT2_Consequent(  "LowTip",lowTipMF, tip )
+unfriendlyServiceUMF = T1MF_Triangular("Upper MF for unfriendly service",0.0,0.0,8.0)
+unfriendlyServiceLMF = T1MF_Triangular("Lower MF for unfriendly service",0.0,0.0,6.0)
+unfriendlyServiceIT2MF = IntervalT2MF_Triangular("IT2MF for unfriendly service",unfriendlyServiceUMF,unfriendlyServiceLMF)
+unfriendlyServiceMF = GenT2MF_Triangular("zGT2MF for unfriendly service", primer = unfriendlyServiceIT2MF, numberOfzLevels = numberOfzLevels)
+unfriendlyService =  GenT2_Antecedent("UnfriendlyService",unfriendlyServiceMF, service)
+
+highTipUMF = T1MF_Gaussian("Upper MF high tip", 30.0, 6.0)
+highTipLMF = T1MF_Gaussian("Lower MF high tip", 30.0, 4.0)
+highTipIT2MF = IntervalT2MF_Gaussian("IT2MF for high Tip",highTipUMF,highTipLMF) 
+highTipMF = GenT2MF_Gaussian("zGT2MF for high tip", highTipIT2MF, numberOfzLevels)
+highTip =  GenT2_Consequent("HighTip",highTipMF, tip )
+
+r1 = GenT2_Rule([badFood, unfriendlyService], consequent = highTip)
+r2 = GenT2_Rule([badFood, unfriendlyService], consequent = lowTip)
+print(r2.equals(r1))
