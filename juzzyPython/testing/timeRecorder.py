64b815fe50107658392117e03f4f64147540ad9e
Sameer Ahmad <sam.ahmad.mc@gmail.com>
2022-03-13 11:48:50 +0000

    pytest

--- juzzyPython/testing/timeRecorder.py
+++ juzzyPython/testing/timeRecorder.py
@@ -5,5 +5,5 @@ def timeDecorator(function):
         start = time.time()
         results = function(*args, **kwargs)
         #print(results)
-        print("Time taken to execute function " + function.__name__ + " is "+ str(time.time()-start))
+        print("Time taken to execute function " + function.__name__ + " is "+ str(time.time()-start) + " seconds.")
     return timeWrapper
